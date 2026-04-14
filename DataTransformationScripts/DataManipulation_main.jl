a61dd9954e080fbc452bd17fcdb26434dd0bb9c9
LukasEgkher22 <lukas@egkher.at>
2026-04-14 11:16:36 +0200

    add new data manipulation for combining trips

--- DataTransformationScripts/DataManipulation_main.jl
+++ DataTransformationScripts/DataManipulation_main.jl
@@ -8,22 +8,25 @@ file_path_passenger = normpath(joinpath(parent_dir, "Data", "Passagertal_0206202
 file_path_timetable = normpath(joinpath(parent_dir, "Data", "DLK_Timetable.xml"))
 file_path_infrastructure = normpath(joinpath(parent_dir, "Data", "dlkinfra_IF-26_20260121081306.xml"))
 file_path_station_abbrev = normpath(joinpath(parent_dir, "Data", "Station Abbreviation.xlsx"))
+file_path_BASEDAY = normpath(joinpath(parent_dir, "Data", "Base Day TUE.xlsx"))
 
 # read in the data and save to CSV for easier access in the future
-df_passenger = parse_passenger_xml(file_path_passenger, save_to_csv = true, filename = "passenger_data.csv")
-df_timetable = parse_timetable_xml(file_path_timetable, save_to_csv = true, filename = "timetable_data.csv")
-df_infra = parse_infrastructure_xml(file_path_infrastructure, save_to_csv = true, filename = "infrastructure_data.csv")
-stations_abbrev = extract_station_names(file_path_station_abbrev, save_to_csv = true, filename = "StationAbbreviation.csv")
+df_passenger = parse_passenger_xml(file_path_passenger)
+df_timetable = parse_timetable_xml(file_path_timetable)
+df_infra = parse_infrastructure_xml(file_path_infrastructure)
+stations_abbrev = extract_station_names(file_path_station_abbrev)
 
 # MANIPULATION of data based on needs
 # creates the main DataFrame with all relevant information for the model
-df_new = merge_data(df_timetable, df_passenger, df_infra, save_to_csv = true, filename = "merged_data.csv")
+df_new = merge_data(df_timetable, df_passenger, df_infra)
+
+result = aggregate_train_trips(df_new, file_path_BASEDAY, save_to_csv=true, filename="aggregated_trips.csv")
 
 # builds the routes DataFrame from the timetable data
-routes = build_route_map(df_timetable, save_to_csv = true, filename = "train_routes.csv")
+# routes = build_route_map(df_timetable, save_to_csv = true, filename = "train_routes.csv")
 
 # builds the connections DataFrame from the merged data
-df_connections = build_connections(df_new, save_to_csv = true, filename = "connections.csv")
+# df_connections = build_connections(df_new, save_to_csv = true, filename = "connections.csv")
 
 # println(first(df_new, 5))
 # println(first(routes, 5))
